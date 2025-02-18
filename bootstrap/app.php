3d47fa16fe705749f7d5bc22c6aaeb24275a80c4
Your Name <you@example.com>
2025-02-18 16:32:29 +0530

    Updated features

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Ensure API requests bypass CSRF
         $middleware->prependToGroup('api', \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
